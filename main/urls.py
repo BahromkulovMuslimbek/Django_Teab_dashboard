7c0c28346b0819f7bddbe58e93312c021a98bfa3
BahromkulovMuslimbek <muslimbahromkulov145@gmail.com>
2024-08-05 10:57:00 +0300

    first commit

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('teashop/', views.teashop, name='teashop'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('testimonies/', views.testimonies, name='testimonies'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,21 @@ from . import models
 
 def index(request):
     return render(request, 'front/index.html')
+
+def about(request):
+    return render(request, 'front/about.html')
+
+def contact(request):
+    return render(request, 'front/contact.html')
+
+def pricing(request):
+    return render(request, 'front/pricing.html')
+
+def services(request):
+    return render(request, 'front/services.html')
+
+def teashop(request):
+    return render(request, 'front/teashop.html')
+
+def testimonies(request):
+    return render(request, 'front/testimonies.html')
