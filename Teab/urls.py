e74b5c408d5c4dc243589007353ac351e50c6384
BahromkulovMuslimbek <muslimbahromkulov145@gmail.com>
2024-08-06 03:36:48 +0300

    second commit

--- Teab/urls.py
+++ Teab/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
